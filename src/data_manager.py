cb166736c2de20a13b98e0d06e99323bc16ee3b3
Kosuke Konno <kosukekonno@KosukenoMacBook-Pro.local>
2024-07-28 09:26:10 +0900

    add experimental files for ml workflow

--- src/data_manager.py
+++ src/data_manager.py
@@ -42,3 +42,53 @@ manager.save_data("initial_crypto_data.csv")  # データの保存
 # ある時間が経過した後にデータを更新
 manager.update_data()
 manager.save_data("updated_crypto_data.csv")  # 更新データの保存
+
+if __name__ == "__main__":
+    from datetime import datetime
+    import time
+
+    import requests
+    import pandas as pd
+
+    url = "https://api.bybit.com/v5/market/kline"
+    symbol = "BTCUSDT"
+    # spot,linear,inverse
+    category = "linear"
+    # 1,3,5,15,30,60,120,240,360,720,D,M,W
+    interval = 15
+
+    timestamp = int(time.time())
+    values = []
+
+    while True:
+        params = {
+            "symbol": symbol,
+            "interval": interval,
+            "category": category,
+            "start": (timestamp - 200 * 60 * interval) * 1000,
+            "end": timestamp * 1000,
+            "limit": 200,
+        }
+
+        response = requests.get(url, params=params)
+        response_data = response.json()
+
+        if len(response_data["result"]["list"]) == 0:
+            break
+
+        values += response_data["result"]["list"]
+        timestamp -= 200 * 60 * interval
+
+    data = pd.DataFrame(values)
+    data.to_csv(f"bybit_{symbol}_{category}_{interval}.csv", index=False)
+
+    # カラム名を修正
+    data.columns = ["timestamp", "open", "high", "low", "close", "volume", "turnover"]
+    # タイムスタンプをdatetime型に変換
+    data["datetime"] = (data["timestamp"].astype(int) / 1000).apply(
+        datetime.fromtimestamp
+    )
+    # 昇順に並び替え
+    data.sort_values("datetime", inplace=True)
+    # "datetime"をインデックスに設定
+    data.set_index("datetime", inplace=True)
